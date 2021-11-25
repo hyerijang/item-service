0e8b379e7a4727924ed9d48e807ec3756b85fb8b
Hyeri Jang <chjang5621@naver.com>
2021-11-25 22:42:50 +0900

    상품 수정

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -72,8 +72,24 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
 
-    // 테스트용
+    @PostMapping("/{itemId}/edit")
+    public String edit(
+            @PathVariable Long itemId,
+            @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /**
+     * 테스트 용 데이터 추가
+     */
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("달고나", 1000, 33));
